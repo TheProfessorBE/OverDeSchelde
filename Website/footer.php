5cde5d7b291678afa3985d5b3a5733e1c1e92e9b
jsteckel2 <34736893+jsteckel2@users.noreply.github.com>
2023-04-26 18:43:28 +0200

    rework step 2

--- Website/footer.php
+++ Website/footer.php
@@ -6,15 +6,16 @@
 
     <div class="bg-ODS-100 rounded-lg p-2">
         <div class=" py-1"></div>
-        <script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#29abe0', 'B0B2KDGXB');kofiwidget2.draw();</script> 
-        <div class=" py-1"></div>
         <div class="text-s text-ODS2-600 flex items-center justify-center space-x-6">
             <p class="mr-2">
-                Designed by <a href="https://github.com/TheProfessorBE" target="_blank" class="text-ODS2-600 font-bold">TheProfessorBE</a>.</br>
-                <a class="text-ODS2-600 font-bold" target="_blank" href="https://ko-fi.com/theprofessorbe">Donaties</a> om de kosten te dekken wordt zeker geäpprecieerd! 
+                Designed by <a href="https://github.com/TheProfessorBE" target="_blank" class="text-ODS2-600 font-bold underline">TheProfessorBE</a>.</br>
+                <a class="text-ODS2-600 font-bold underline" target="_blank" href="https://ko-fi.com/theprofessorbe">Donaties</a> om de kosten te dekken wordt zeker geäpprecieerd! 
             </p>
-            <!--<img class="rounded-full w-14 h-auto shadow-lg" src="images/cartoon_small.jpg"></img>-->
-        </div>    
+        </div>   
+        <div class=" py-1"></div>
+        <script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#29abe0', 'B0B2KDGXB');kofiwidget2.draw();</script> 
+        
+ 
     </div>
     <div class="bg-ODS-100 rounded-lg p-2">
         
@@ -34,7 +35,7 @@
 <div class=" py-1"></div>
 <div class="text-xs text-ODS-600 bg-ODS-100 rounded-lg p-2 text-justify">
     <p>
-    <b>Disclaimer:</b> This website is a hobby project by <a href="https://github.com/TheProfessorBE" target="_blank" class="text-ODS2-600 font-bold">TheProfessorBE</a>, not a professional website. The information provided on this website is for general informational purposes only. While we strive to 
+    <b>Disclaimer:</b> This website is a hobby project by <a href="https://github.com/TheProfessorBE" target="_blank" class="text-ODS2-600 font-bold underline">TheProfessorBE</a>, not a professional website. The information provided on this website is for general informational purposes only. While we strive to 
     ensure that the information is up-to-date and accurate, we make no representations or warranties of any kind, express or 
     implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information.
     Any reliance you place on such information is therefore strictly at your own risk. We will not be liable for any loss or damage arising 
