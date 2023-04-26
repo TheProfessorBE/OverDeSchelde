606adf4dfc02c702eb783862c920dab97f8c74c4
jsteckel2 <34736893+jsteckel2@users.noreply.github.com>
2023-04-26 14:41:03 +0200

    restyle of installation,

--- Website/installation.php
+++ Website/installation.php
@@ -8,31 +8,34 @@
             <div class="flex flex-col text-white ">
                 <?php include 'navbar.php';?>
 
-                <h1 class="my-6 text-center text-4xl font-semibold text-ODS-800">Over de Schelde?</h1>
+                <h1 class="my-2 text-center text-4xl font-semibold text-ODS-800 bg-ODS-100 rounded-lg p-2">Over de Schelde?</h1>
                 
-                <div class="grid text-ODS-900 text-lg">
-                    <h2 class="my-6 text-left text-2xl font-semibold text-ODS-800">Waarom deze website?</h2>
-                    <p class="text-justify"> Deze website is gebouwd door een gebruiker van het veer, om op een overzichtelijke manier de uren van de verschillende veerboten over de Schelde te kunnen raadplegen.
+                <div class="grid text-ODS-900 text-lg bg-ODS-100 rounded-lg p-2">
+                    <h2 class="m2-6 text-left text-2xl font-semibold text-ODS-800 mx-2">Waarom deze website?</h2>
+                    <p class="text-justify p-2"> Deze website is gebouwd door een gebruiker van het veer, om op een overzichtelijke manier de uren van de verschillende veerboten over de Schelde te kunnen raadplegen.
                             Uit een eerste versie voor eigen gebruik is dan overdeschelde.be gegroeid, om het voor het bredere publiek bruikbaar te maken. Deze website is volledig een hobby project
                             waarmee geen geld verdiend wordt. Donaties om de website operationeel te houden worden geäpprecieerd, en dienen om de lopende kosten te dekken. Donaties kunnen via het Ko-Fi platform op <a class="text-ODS-900 font-bold" target="_blank" href="https://ko-fi.com/theprofessorbe">https://ko-fi.com/theprofessorbe</a>. 
                             We doen ons best om de informatie zo correct mogelijk te houden, maar garantie kan hierop niet gegeven worden. Bij twijfel kan je best steeds de websites van de vloot nakijken.
                     </p>
                 </div>
                 
-                <h2 class="my-6 text-left text-2xl font-semibold text-ODS-800">Installatie Android</h2>
-                <div class=" lg:flex space-x-4  text-ODS-900 text-lg ">
-                    <p class="text-justify">
-                        Er zijn enkele verschillende mogelijkheden om Over de Schelde te installeren op je smartphone. Voor Android telefoons is de meest eenvoudige manier via de Google play store. De applicatie kan je
-                        hier vinden: <a class="text-ODS-900 font-bold" target="_blank" href="https://play.google.com/store/apps/details?id=be.overdeschelde.twa">Over De Schelde</a>
-                        <br><br>
-                        Je kan de applicatie ook via de website installeren. Als je de hoofdpagina van de website opent op een smartphone zal je browser automatisch vragen om de app te installeren. Door op deze prompt te klikken kan je de applicatie op je smartphone desktop installeren. 
-                        Indien je deze prompt niet krijgt kan je ook via Share - Add to homescreen (IOS) of de drie bolletjes - Install app (Android) de installatie voltooien.
-                        <br/>
-                        <br/>
-                        Je kan er ook voor kiezen om een widget applicatie te installeren die
-                        van een website een desktop widget kan maken. Widgetify is zo een applicatie voor android, en kan hier gedownloaded worden: <a href="https://play.google.com/store/apps/details?id=com.binarysmith.webclipwidget.ad">Widgetify</a>.
-                    </p>                   
-                    <img src="Widgetify_small.jpeg" alt="Installation App" class="h-100  rounded-lg flex.flex-col.gap-4 w-1/2 lg:w-1/6">
+                <div class="my-2 bg-ODS-100 rounded-lg p-2">
+                    <h2 class="text-left text-2xl font-semibold text-ODS-800 p-2">Installatie Android</h2>
+                    <div class=" lg:flex space-x-4  text-ODS-900 text-lg ">
+                        <p class="text-justify mx-2">
+                            Er zijn enkele verschillende mogelijkheden om Over de Schelde te installeren op je smartphone. Voor Android telefoons is de meest eenvoudige manier via de Google play store. De applicatie kan je
+                            hier vinden: <a class="text-ODS-900 font-bold" target="_blank" href="https://play.google.com/store/apps/details?id=be.overdeschelde.twa">Over De Schelde</a>
+                            <br><br>
+                            Je kan de applicatie ook via de website installeren. Als je de hoofdpagina van de website opent op een smartphone zal je browser automatisch vragen om de app te installeren. Door op deze prompt te klikken kan je de applicatie op je smartphone desktop installeren. 
+                            Indien je deze prompt niet krijgt kan je ook via Share - Add to homescreen (IOS) of de drie bolletjes - Install app (Android) de installatie voltooien.
+                            <br/>
+                            <br/>
+                            Je kan er ook voor kiezen om een widget applicatie te installeren die
+                            van een website een desktop widget kan maken. Widgetify is zo een applicatie voor android, en kan hier gedownloaded worden: <a href="https://play.google.com/store/apps/details?id=com.binarysmith.webclipwidget.ad">Widgetify</a>.
+                        </p>                   
+                        <img src="Widgetify_small.jpeg" alt="Installation App" class="h-100  rounded-lg flex.flex-col.gap-4 w-1/2 lg:w-1/6">
+                    </div>
+
                 </div>
 
                 <?php include 'footer.php';?>                
