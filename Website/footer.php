0d18eaf32cc6fa9bfb0405a26f6acba72f85e613
jsteckel2 <34736893+jsteckel2@users.noreply.github.com>
2023-05-14 11:03:57 +0200

    some additional things

--- Website/footer.php
+++ Website/footer.php
@@ -33,12 +33,14 @@
 </div>
 
 <div class=" py-1"></div>
-<div class="text-xs text-ODS-600 bg-ODS-100 rounded-lg p-2 text-justify">
+<div class="text-xs text-ODS-600 bg-ODS-100 rounded-lg p-2 text-center">
     <p>
     <b>Disclaimer:</b> This website is a hobby project by <a href="https://github.com/TheProfessorBE" target="_blank" class="text-ODS2-600 font-bold underline">TheProfessorBE</a>, not a professional website. The information provided on this website is for general informational purposes only. While we strive to 
     ensure that the information is up-to-date and accurate, we make no representations or warranties of any kind, express or 
     implied, about the completeness, accuracy, reliability, suitability, or availability with respect to the website or the information.
     Any reliance you place on such information is therefore strictly at your own risk. We will not be liable for any loss or damage arising 
     from the use of this website or the information provided.
+    <br/>
+    Contact us via <a href="mailto:info@overdeschelde.be" target="_blank" class="text-ODS2-600 font-bold underline">info@overdeschelde.be</a>.
     </p>
 </div>
